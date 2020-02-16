70393125c385a9189b0f4293a3e412fd84f91aae
Eastan Giebler <kupa528@yahoo.com>
2020-02-15 22:34:33 -0600

    Fix up converter script

--- transcrive/PDFtoPNG.py
+++ transcrive/PDFtoPNG.py
@@ -4,16 +4,13 @@ import pathlib
 import convertapi as convertapi
 
 
-def main():
-    filename = 'test.pdf'
+def convert(filename):
 
     parent_dir = str(pathlib.Path(__file__).parent.absolute())
 
     convertapi.api_secret = 'HRfgvNWaFLiw4U2w'
     convertapi.convert('jpg', {
-        'File': os.path.join(parent_dir, 'test.pdf')
+        'File': os.path.join(parent_dir, filename)
     }, from_format='pdf').save_files(os.path.join(parent_dir, "output"))
 
 
-if __name__ == "__main__":
-    main()
