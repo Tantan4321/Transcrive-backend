6197c5015d58dbe379a4211c64e974d22e450e0b
Archit Das <archit.das@outlook.com>
2020-02-15 20:11:29 -0600

    PDF to Images script

--- transcrive/PDFtoPNG.py
+++ transcrive/PDFtoPNG.py
@@ -1,11 +1,20 @@
+import os
+import pathlib
+import tempfile
 from pdf2image import convert_from_path
-
 def main():
-    pdf_path = "C:\Git Stuff\Transcrive-backend\Input\Is this Reality.pdf"
+    filename = 'test.pdf'
 
+    with tempfile.TemporaryDirectory() as path:
+        images_from_path = convert_from_path(filename, output_folder=path)
 
-    convert_from_path(pdf_path, output_folder="C:\Git Stuff\Transcrive-backend\Output")
+    base_filename = os.path.splitext(os.path.basename(filename))[0] + '.png'
 
+    parent_dir = str(pathlib.Path(__file__).parent.absolute())
+    save_dir = 'output'
+
+    for page in images_from_path:
+        page.save(os.path.join(parent_dir, save_dir, base_filename), 'PNG')
 
 if __name__ == "__main__":
     main()
